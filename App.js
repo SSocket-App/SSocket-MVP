59572d3d64f2179f89a3ff4418d2bf065f016f8c
SSocket-App <ssocketapp@gmail.com>
2025-09-04 19:06:49 -0700

    feat(nav): update App entrypoint to use AppNavigator

--- App.js
+++ App.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
-import AppNavigator from './AppNavigator';
+import AppNavigator from  ./src/navigation/AppNavigator'
 
 export default function App() {
-  return (
-    <>
-      <StatusBar barStyle="light-content" />
-      <AppNavigator />
-    </>
-  );
+  return <AppNavigator />;
 }
